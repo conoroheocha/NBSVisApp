1c3e07dcbab438187b08b4af07bc19e975f7e3de
conoroheocha <conoroheocha@gmail.com>
2022-04-18 20:53:25 +0100

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import 'leaflet/dist/leaflet.css';
-import Navigation from './navigation';
-
-export function App() {
-  return <Navigation />
-}
